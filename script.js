6d5140985e4adeafae2859fa54c4a045f4507f85
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2023-04-21 09:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let input = document.getElementById('newTodoInput');
+let todoList = document.getElementById('todoList');
+
+
+function addTodo(){
+	let inputValue = input.value;
+	todoList.append(inputValue);
+}
+addTodoBtn.addEventListener('click', addTodo)
+
 
